6b00c2660fe5c7e92dea2963e6eafda2ac250c19
Dashsoap <360062589@qq.com>
2019-05-09 16:08:56 +0800

    BAKERY

--- src/bakery/bakery.controller.ts
+++ src/bakery/bakery.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { BakeryService } from './bakery.service';
 
 @Controller('bakery')
-export class BakeryController {}
+export class BakeryController {
+    constructor(private readonly bakeryService: BakeryService) { }
+
+    @Get()
+    async getAllbakery() {
+        return await this.bakeryService.getAllBakery()
+    }
+
+    @Post()
+    async addBakery(@Body() body) {
+        return await this.bakeryService.createBakery(body)
+    }
+    @Get(":id")
+    async getBakery(@Param() param) {
+        return await this.bakeryService.getBakery(param.id)
+    }
+}
--- src/bakery/bakery.service.ts
+++ src/bakery/bakery.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { BakeryEntity } from './bakery.entity';
+import { DeepPartial } from 'typeorm';
 
 @Injectable()
-export class BakeryService {}
+export class BakeryService {
+    async getAllBakery() {
+        return await BakeryEntity.find();
+    }
+
+    async createBakery(vo) {
+        const bakery = new BakeryEntity();
+        for (var i in vo) {
+            bakery[i] = vo[i];
+        }
+        return bakery.save();
+    }
+
+    async getBakery(id) {
+        return await BakeryEntity.findOne(id)
+    }
+}
